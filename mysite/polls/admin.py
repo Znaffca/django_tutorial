207477642ff07ba05f768e3dd95f547a8c1d58e6
Grzegorz Salacinski <salacinski.grzegorz@gmail.com>
2019-06-05 18:09:11 +0200

    Sample modification of admin page template

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    """group fields in fieldsets"""
+    fieldsets = [
+        (None,  {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+    """add to Question add/modify form a foreignkey view and edit"""
+    inlines = [ChoiceInline]
+
+    """change default display of Question objects"""
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    """add simple filter by pub_date"""
+    list_filter = ['pub_date']
+
+    """add simple search field in top of site, search in question_text"""
+    search_fields = ['question_text']
+
+    """simple pagination"""
+    list_per_page = 5
+
+admin.site.register(Question, QuestionAdmin)
